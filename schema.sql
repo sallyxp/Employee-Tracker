2c6d1fde8e342eec0e283412e16e53d5ad5c491b
Sally Rodgers <sallyxp@yahoo.co.uk>
2021-04-22 01:57:46 +0100

    amended formatting - improved layout

--- schema.sql
+++ schema.sql
@@ -19,7 +19,7 @@ CREATE TABLE role (
 );
 
 CREATE TABLE employee (
-   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   first_name VARCHAR (38),
   last_name VARCHAR (30),
   role_id INT,
